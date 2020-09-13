ccf09ef27a5ef4cc7e2c8b774a7eb382d844506a
Pavlo Minyuk <minukpasa@gmail.com>
2020-09-13 19:43:20 +0300

    123 456

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -15,7 +15,10 @@ public class TestApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here        
+        // TODO code application logic here    
+		float a=10;
+		float b=(float) 12.5;
+		System.out.println("a+b="+(a+b));
         System.out.println("GIT test application");
     }
     
