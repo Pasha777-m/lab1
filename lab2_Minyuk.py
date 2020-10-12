1839a2c72cc1433873a010202bfe31dfda7be0c7
Pavlo Minyuk <minukpasa@gmail.com>
2020-10-12 18:55:37 +0300

    12345678910111213

--- lab2_Minyuk.py
+++ lab2_Minyuk.py
@@ -1,33 +1,55 @@
-from collections import deque # імпортування бібліотеки deque
-import unittest #імпортування бібліотеки unittest для подальшої реалізації тестів
-a=[5,-9,756,86,-48,9,7] #створення статичного масиву
-class myClass(): #створення загального класу
-    def array(): #створення функції
-        sum = 0 #декларуємо sum
-        k = 0 #декларуємо k
-        for i in range(len(a)): #цикл для визначення довжини масиву а
-            sum  = a[i] #визначення всіх елементів масиву
-            k += 1 #шукаємо довжину масиву
-        print(a) #виводимо наш масив
-        return k #повертаємо k
-    print("кількість елементів в списку:", array()) #виводимо к-ть елементів масиву
-    def queue(a): #створення функції
-        d = deque(a, maxlen=7) #створення черги
-        sum1 = 0 #декларуємо sum1
-        p = 0 #декларуємо р
-        for i in range(len(d)): #цикл для визначення довжини черги d
-            sum1  = d[i] #визначення всіх елементів черги
-            p += 1  #шукаємо довжину черги       
-        print(d) #виводимо нашу чергу
-        return p #повертаємо р
-    print("кількість елементів в черзі:", queue(a)) #виводимо к-ть елементів черги
+from collections import deque 
+import unittest
 
-class MyTest(unittest.TestCase): #створення класу для реалізації тестів
-    def test_usage1(self):#створення функції для тесту
-        self.assertEqual(myClass.array(),7)#використання команди self.assertEqual() для порівняння довжини масиву
+a=[5,-9,756,86,-48,9,7]
+d = deque(a, maxlen=7)
+a1=[]
+b = deque(a1)
 
-    def test_usage2(self):#створення функції для тесту 2 
-        self.assertEqual(myClass.queue(a),7)#використання команди self.assertEqual() для порівняння довжини черги
+class myClass():
+    
+    def array(): 
+        sum = 0 
+        k = 0 
+        for i in range(len(a)): 
+            sum  = a[i] 
+            k += 1 
+        print(a) 
+        return k    
+    print("кількість елементів в списку:", array())
+    
+    def queue(a): 
+        sum1 = 0 
+        p = 0 
+        for i in range(len(d)):
+            sum1  = d[i] 
+            p += 1         
+        print(d)
+        return p    
+    print("кількість елементів в черзі:", queue(a))
+    
+    def add(d):
+        d.append(777)
+        print(d)
+        return d
+
+    def empty(b):
+        d.pop()
+        print(b)
+        return b
+        
+class MyTest(unittest.TestCase): 
+    def test_usage1(self):
+        self.assertEqual(myClass.array(),7)
+
+    def test_usage2(self):
+        self.assertEqual(myClass.queue(a),7)
+
+    def test_usage3(self):
+        self.assertTrue(myClass.add(d))
+
+    def test_usage4(self):
+        self.assertFalse(myClass.empty(b))
         
 if __name__ == "__main__":
-    unittest.main()#команда яка запускає всі тести із заданого модуля
+    unittest.main()
