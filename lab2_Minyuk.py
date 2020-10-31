1dc1ad3aa3c9670f54c6856f6b80f98fe8248ef3
Pavlo Minyuk <minukpasa@gmail.com>
2020-10-31 21:30:51 +0200

    3456789

--- lab2_Minyuk.py
+++ lab2_Minyuk.py
@@ -1,5 +1,114 @@
 from collections import deque 
 import unittest
+import random
+
+a = [5,-9,756,86,-48,9,7]
+a1 = []
+d = deque(a, maxlen=7)
+d1 = deque(a1)
+
+class myClass():
+    
+    def array(): 
+        sum = 0 
+        k = 0 
+        for i in range(len(a)): 
+            sum  = a[i] 
+            k += 1 
+        print(a) 
+        return k    
+    print("кількість елементів в списку:", array())
+    
+    def queue(a): 
+        sum1 = 0 
+        p = 0 
+        for i in range(len(d)):
+            sum1  = d[i] 
+            p += 1         
+        print(d)
+        return p    
+    print("кількість елементів в черзі:", queue(a))
+
+    def add(d):
+        try:
+            d.insert(0, -777)
+        except Exception:
+            print(" ")
+    #print(add(d))
+
+    def remove(d1):
+        try:
+            d1.pop()
+        except Exception:
+            print(" ")
+    #print(remove(d1))
+            
+        
+    
+class MyTest(unittest.TestCase): 
+    def test_usage1(self):
+        self.assertEqual(myClass.array(),7)
+
+    def test_usage2(self):
+        self.assertEqual(myClass.queue(a),7)
+
+    def test_usage3(self):
+        self.assertFalse(myClass.add(d))
+        
+    def test_usage4(self):
+        self.assertFalse(myClass.remove(d1))
+        
+if __name__ == "__main__":
+    unittest.main()
+
+    
+'''
+from collections import deque 
+import unittest
+import random
+
+a=[5,-9,756,86,-48,9,7]
+d = deque(a, maxlen=7)
+
+class myClass():
+    
+    def array(): 
+        sum = 0 
+        k = 0 
+        for i in range(len(a)): 
+            sum  = a[i] 
+            k += 1 
+        print(a) 
+        return k    
+    print("кількість елементів в списку:", array())
+    
+    def queue(a): 
+        sum1 = 0 
+        p = 0 
+        for i in range(len(d)):
+            sum1  = d[i] 
+            p += 1         
+        print(d)
+        return p    
+    print("кількість елементів в черзі:", queue(a))
+        
+class MyTest(unittest.TestCase): 
+    def test_usage1(self):
+        self.assertEqual(myClass.array(),7)
+
+    def test_usage2(self):
+        self.assertEqual(myClass.queue(a),7)
+
+        
+        
+if __name__ == "__main__":
+    unittest.main()
+'''
+
+
+'''
+from collections import deque 
+import unittest
 
 a=[5,-9,756,86,-48,9,7]
 d = deque(a, maxlen=7)
@@ -53,3 +162,4 @@ class MyTest(unittest.TestCase):
         
 if __name__ == "__main__":
     unittest.main()
+'''
